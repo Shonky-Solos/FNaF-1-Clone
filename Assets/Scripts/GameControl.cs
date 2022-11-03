764a5958492913b0602a1835a36bd4911c2ccd80
Shonky-Solos <hartmannsolomon@gmail.com>
2022-11-03 20:18:34 +1300

    Initial Commit
    Here's the project as it stands

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+
+    public int hour;
+    private int currentHour;
+    public float power;
+    public string displayPower;
+    public int usage;
+    public int[] difficulty;
+    public Image[] hourPictures;
+
+    void Start()
+    {
+        //Represented in game as 12am but functionally the first hour
+        usage = 1;
+        hour = -1;
+        currentHour = -1;
+        //Starts power at 100%
+        power = 100;
+        StartCoroutine(DoGame());
+    }
+    void Update()
+    {
+        // Handles changes ON THE HOUR
+        if(currentHour != hour)
+        {
+            //Change the image of the hour (eg 2am)
+            hourPictures[currentHour].enabled = false;
+            hourPictures[hour].enabled = true;
+            // Make sure any changes happen before here
+            currentHour = hour;
+        }
+        //Run down power proportional to power usage by player
+        power -= usage / 6 * Time.deltaTime;
+        //Shows power as a percentage without decimals
+        displayPower = Mathf.FloorToInt(power).ToString();
+    }
+
+    //Called on button press, plays first night with lowest difficulty
+    public void NewGame()
+    {
+        difficulty[0] = 0;
+        difficulty[1] = 0;
+        difficulty[2] = 0;
+        difficulty[3] = 0;
+    }
+
+    public IEnumerator DoGame()
+    {
+        //Updates hour every 90s, or wins/loses game
+        hour += 1;
+        yield return new WaitForSeconds(90);
+        //Increase hour then start timer again
+        if(hour != 6 && power > 0)
+        {
+            StartCoroutine(DoGame());
+        }
+        if(hour == 6)
+        {
+            // WIN THE GAME
+        }
+        if(hour != 6 && power <= 0)
+        {
+            // LOSE
+        }
+    }
+}
---
+++ Assets/Scripts/MainEnemy.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainEnemy : MonoBehaviour
+{
+
+    #region Variables
+    public int difficulty;
+    public float moveTimer;
+    public int[] location;
+    public bool camUse;
+    private bool canMove;
+    private bool aiChange;
+    private int hour;
+    private int currentHour;
+    private GameControl gc;
+    #endregion
+
+    void Start()
+    {
+        //Make the AI not move immediately upon start
+        canMove = false;
+
+        //Read the GameControl script for what AI level to set
+        if(gameObject.name == "Bonnie")
+        {
+            difficulty = gc.difficulty[0];
+        }
+        if(gameObject.name == "Chica")
+        {
+            difficulty = gc.difficulty[1];
+        }
+        if (gameObject.name == "Freddy")
+        {
+            difficulty = gc.difficulty[2];
+        }
+        if (gameObject.name == "Foxy")
+        {
+            difficulty = gc.difficulty[3];
+        }
+    }
+
+    void Update()
+    {
+        //Use GameControl's hour
+        hour = gc.hour;
+        
+        //Run AI movement check
+        if (canMove)
+        {
+            StartCoroutine(Movement());
+        }
+
+        //Run processes that only update per hour
+        if(currentHour != hour)
+        {
+            //Increase difficulty throughout night
+            if(hour == 2)
+            {
+                if(gameObject.name == "Bonnie")
+                {
+                    difficulty += 1;
+                }
+            }
+            //These increase in AI level at these specific hours
+            if(hour == 3 || hour == 4)
+            {
+                if(gameObject.name == "Bonnie" || gameObject.name == "Chica" || gameObject.name == "Foxy")
+                {
+                    difficulty += 1;
+                }
+            }
+
+            //Do these updates once then wait until the next hour
+            currentHour = hour;
+        }
+    }
+
+    //copied over from FNaF, staggers the enemies
+    public IEnumerator Setup()
+    {
+        yield return new WaitForSeconds(moveTimer);
+        canMove = true;
+    }
+
+    public IEnumerator Movement()
+    {
+        canMove = false;
+        //AI can only move if their AI is better than a d20 roll
+        if(difficulty >= Random.Range(1, 20))
+        {
+            //DO MOVEMENT - Haven't solved yet
+        }
+        //Prevents AI from moving for their move timer
+        yield return new WaitForSeconds(moveTimer);
+        //Lets them roll to move again
+        canMove = true;
+    }
+}
